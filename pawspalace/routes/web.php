59d877de748ff32931c621c520f0d7548157c578
Alejandro Torres <edialeto@hotmail.com>
2024-03-27 15:28:55 -0500

    Login added

--- pawspalace/routes/web.php
+++ pawspalace/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home Controller routes
+// User routes
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+
+// Authentication Controllers routes
+Auth::routes();
