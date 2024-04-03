2520fed93a087eae0c38b607431eddfe3361e183
Alejandro Torres <edialeto@hotmail.com>
2024-04-02 22:54:34 -0500

    Design updated

--- pawspalace/resources/views/product/favorite.blade.php
+++ pawspalace/resources/views/product/favorite.blade.php
@@ -13,7 +13,7 @@
         @foreach ($favorites as $favorite)
         <div class="col-md-3 mb-4">
             <div class="card">
-                <img src="{{ asset('/storage/' . $favorite->image) }}" class="card-img-top" alt="{{ $favorite->name }}">
+                <img src="{{ asset('/storage/' . $favorite->image) }}" class="card-img-top" alt="{{ $favorite->name }}" style="width: 100%; height: 100%; object-fit: contain; max-height: 180px;">
                 <div class="card-body text-center">
                     <h5 class="card-title">{{ $favorite->name }}</h5>
                     <a href="{{ route('product.show', ['id' => $favorite->id]) }}" class="custom-button">View
