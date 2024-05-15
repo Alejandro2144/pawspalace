82dc87ca2f0e897b124ccdbac9322dc28498805b
Katherine <kbenjumeao@eafit.edu.co>
2024-05-15 10:19:55 -0500

    Corrections in models 2

--- pawspalace/app/Models/Item.php
+++ pawspalace/app/Models/Item.php
@@ -20,7 +20,6 @@ class Item extends Model
      * $this->product - Product - contains the associated Product
      * $this->appointment - Appointment - contains the associated Appointment
      */
-    
     public function getId(): int
     {
         return $this->attributes['id'];
