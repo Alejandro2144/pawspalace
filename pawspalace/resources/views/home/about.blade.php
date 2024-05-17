d8d4c2571c761f7832ad2aba85557a97b8f57599
Katherine <kbenjumeao@eafit.edu.co>
2024-05-16 20:05:04 -0500

    About

--- pawspalace/resources/views/home/about.blade.php
+++ pawspalace/resources/views/home/about.blade.php
@@ -5,24 +5,88 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8">
-            <div class="about-section">
-                <h2>Sobre PawsPalace</h2>
-                <p>¡Bienvenido a PawsPalace! Somos una tienda en línea dedicada a productos y servicios para mascotas. Nuestro objetivo es proporcionar a los amantes de las mascotas todo lo que necesitan para cuidar, mimar y mantener felices a sus queridos compañeros peludos.</p>
+            <div class="about-section" id="about-pawspalace">
+                <h1 class="about-title">Sobre PawsPalace</h1>
+                <h4>Nuestra Historia</h4>
+                <p>PawsPalace nació de la pasión compartida de un grupo de personas, Alejandro, Angel y Katherine, por los animales. Después de años de trabajar en diferentes áreas de la industria de mascotas, se dieron cuenta de la necesidad de crear un destino en línea donde los amantes de los animales pudieran encontrar todo lo que necesitan para sus queridos compañeros peludos.</p>
+                <h4>Nuestra Misión y Valores</h4>
+                <p>En PawsPalace, nuestra misión es proporcionar a los dueños de mascotas productos de alta calidad que promuevan la felicidad y el bienestar de sus amigos peludos. Nos comprometemos a ofrecer un servicio excepcional, manteniendo siempre la integridad, la transparencia y el respeto por todos los seres vivos.</p>
+                <h4>Nuestro Equipo</h4>
+                <p>El equipo de PawsPalace está formado por un grupo apasionado de individuos que comparten un amor inquebrantable por los animales. Desde nuestros fundadores hasta nuestro equipo de atención al cliente, cada miembro está dedicado a proporcionar la mejor experiencia posible a nuestros clientes y sus mascotas.</p>
+                <h4>Nuestro Compromiso con la Comunidad</h4>
+                <p>En PawsPalace, creemos en devolver a la comunidad que tanto nos ha dado. Colaboramos con refugios locales de animales, donando una parte de nuestras ganancias y organizando eventos de adopción para ayudar a encontrar hogares amorosos para mascotas necesitadas.</p>
+                <p>PawsPalace ha sido honrado con el Premio a la Excelencia en Servicio al Cliente en la Industria de Mascotas dos años consecutivos, reconociendo nuestro compromiso con la satisfacción del cliente y la calidad del servicio.</p>
+                <p>¡No solo somos nosotros quienes amamos a PawsPalace! Nuestros clientes están encantados con nuestros productos y servicios. '¡La calidad de los productos de PawsPalace es insuperable!' </p>
             </div>
-            <div class="faq-section">
-                <h2>Preguntas Frecuentes</h2>
-                <p>¿Tienes preguntas sobre nuestros productos o servicios? Consulta nuestras preguntas frecuentes para obtener respuestas a tus dudas más comunes.</p>
+
+            <div class="faq-section" id="faq">
+            <h1 class="about-title">Preguntas Frecuentes</h1>
+                <div class="faq-item">
+                    <h5>¿Cuál es el tiempo de entrega estimado?</h5>
+                    <p>El tiempo de entrega varía según tu ubicación. Generalmente, los pedidos se entregan dentro de 3-5 días hábiles.</p>
+                </div>
+                <div class="faq-item">
+                    <h5>¿Qué hago si mi pedido llega dañado?</h5>
+                    <p>Si tu pedido llega dañado, por favor contáctanos de inmediato para que podamos resolver el problema lo antes posible.</p>
+                </div>
+                <div class="faq-item">
+                    <h5>¿Puedo devolver un producto?</h5>
+                    <p>Sí, aceptamos devoluciones dentro de los 30 días posteriores a la recepción del producto. Por favor revisa nuestra política de devoluciones para más detalles.</p>
+                </div>
+                <div class="faq-item">
+                    <h5>¿Ofrecen envío internacional?</h5>
+                    <p>Sí, ofrecemos envío internacional a la mayoría de los países. Los costos y tiempos de envío pueden variar. Por favor, consulta nuestras opciones de envío durante el proceso de compra.</p>
+                </div>
+                <div class="faq-item">
+                    <h5>¿Tienen una tienda física?</h5>
+                    <p>Actualmente contamos con una sede exclusiva para las visitas de nuestros peludos. En la página principal podrás encontrar un mapa con nuestra ubicación</p>
+                </div>
+                <div class="faq-item">
+                    <h5>¿Cómo puedo contactar al servicio al cliente?</h5>
+                    <p>Puedes contactar a nuestro equipo de servicio al cliente por correo electrónico a info@pawspalace.com o llamando al +123456789.</p>
+                </div>
             </div>
-            <div class="privacy-policy-section">
-                <h2>Política de Privacidad</h2>
-                <p>Nuestra política de privacidad detalla cómo recopilamos, usamos y protegemos la información personal de nuestros clientes. Tu privacidad es importante para nosotros.</p>
-            </div>
-            <div class="terms-conditions-section">
-                <h2>Términos y Condiciones</h2>
-                <p>Lee nuestros términos y condiciones para comprender tus derechos y responsabilidades al utilizar nuestro sitio web y comprar nuestros productos.</p>
-            </div>
+        
+        <div class="privacy-policy-section" id="privacy-policy">
+            <h1 class="about-title">Política de Privacidad</h1>
+            <p>En PawsPalace, nos tomamos muy en serio la privacidad de nuestros usuarios. Esta política de privacidad describe cómo recopilamos, utilizamos y protegemos la información personal que nos proporcionas.</p>
+            <h5>Información que recopilamos</h5>
+            <p>Recopilamos información personal cuando te registras en nuestra tienda en línea, realizas un pedido, te suscribes a nuestro boletín informativo o completas un formulario. La información que recopilamos puede incluir tu nombre, dirección de correo electrónico, dirección postal, número de teléfono y detalles de pago.</p>
+            <h5>Cómo utilizamos tu información</h5>
+            <p>Utilizamos la información que recopilamos para procesar tus pedidos, administrar tu cuenta, enviar correos electrónicos promocionales y mejorar nuestros productos y servicios. También podemos utilizar tu información para fines de marketing, pero solo con tu consentimiento explícito.</p>
+            <h5>Protección de la información</h5>
+            <p>Tomamos medidas de seguridad para proteger tu información personal contra pérdida, uso indebido o acceso no autorizado. Utilizamos encriptación SSL para proteger tus datos durante la transmisión y mantenemos procedimientos estrictos de seguridad para proteger tus datos almacenados.</p>
+            <h5>Divulgación a terceros</h5>
+            <p>No vendemos, intercambiamos ni transferimos tu información personal a terceros sin tu consentimiento, excepto cuando sea necesario para cumplir con la ley, hacer cumplir nuestras políticas de sitio web o proteger nuestros derechos, propiedad o seguridad.</p>
+            <h5>Consentimiento</h5>
+            <p>Al utilizar nuestro sitio web, aceptas nuestra política de privacidad y nos das tu consentimiento para recopilar, utilizar y proteger tu información personal de acuerdo con esta política.</p>
+            <h5>Actualizaciones de la política</h5>
+            <p>Nos reservamos el derecho de actualizar o cambiar nuestra política de privacidad en cualquier momento. Cualquier cambio significativo se publicará en esta página. Te recomendamos que revises periódicamente esta página para mantenerte informado sobre cómo protegemos tu información.</p>
+            <h5>Contacto</h5>
+            <p>Si tienes alguna pregunta sobre nuestra política de privacidad, por favor contáctanos a través de info@pawspalace.com.</p>
         </div>
-        <div class="col-lg-4">
+
+        <div class="terms-conditions-section" id="terms-conditions">
+            <h1 class="about-title">Términos y Condiciones</h1>
+            <p>Por favor, lee atentamente estos términos y condiciones antes de utilizar nuestro sitio web.</p>
+            <h5>Aceptación de Términos</h5>
+            <p>Al acceder y utilizar este sitio web, aceptas estar sujeto a estos términos y condiciones y a todas las leyes y regulaciones aplicables. Si no estás de acuerdo con alguno de estos términos, no utilices nuestro sitio.</p>
+            <h5>Uso del Sitio</h5>
+            <p>El contenido de este sitio web es solo para información general y uso personal. Nos reservamos el derecho de modificar o retirar temporal o permanentemente el sitio (o cualquier parte de él) con o sin previo aviso.</p>
+            <h5>Propiedad Intelectual</h5>
+            <p>Todos los derechos de propiedad intelectual en este sitio web, incluidos, entre otros, el diseño, el texto, los gráficos, el software y todos los códigos fuente subyacentes, son propiedad nuestra o de nuestros proveedores.</p>
+            <h5>Enlaces a Terceros</h5>
+            <p>Nuestro sitio web puede contener enlaces a sitios web de terceros. Estos enlaces se proporcionan únicamente para tu conveniencia y no implican que respaldemos o tengamos ninguna asociación con el sitio enlazado.</p>
+            <h5>Limitación de Responsabilidad</h5>
+            <p>No seremos responsables de ningún daño directo, indirecto, incidental, especial, emergente o punitivo que surja de tu uso o acceso a este sitio web.</p>
+            <h5>Modificaciones</h5>
+            <p>Nos reservamos el derecho de modificar estos términos y condiciones en cualquier momento. Las modificaciones serán efectivas inmediatamente después de su publicación en el sitio web.</p>
+            <h5>Ley Aplicable</h5>
+            <p>Estos términos y condiciones se regirán e interpretarán de acuerdo con las leyes del país en el que estamos ubicados.</p>
+            <p>Si tienes alguna pregunta sobre estos términos y condiciones, contáctanos a través de info@pawspalace.com.</p>
+        </div>
+    </div>
+    <div class="col-lg-4">
             <div class="additional-info">
                 <h3>Información Adicional</h3>
                 <ul>
@@ -35,4 +99,5 @@
         </div>
     </div>
 </div>
+
 @endsection
